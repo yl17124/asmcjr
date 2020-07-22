1f40a3ddc513b1f2855b7a7566415eee713594aa
David Liao <35374714+yl17124@users.noreply.github.com>
2020-07-22 08:30:56 +0100

    Update header.R

--- images/header.R
+++ images/header.R
@@ -1,9 +1,9 @@
 .onAttach <- function(...) {
 
-   packageStartupMessage("\n ## This package supports the book Analyzing Spatial Models of Choice and Judgment with R") 
+   packageStartupMessage("\n ## This package supports the book Analyzing Spatial Models of Choice and Judgment") 
    packageStartupMessage("\n ## Authors: David A. Armstrong, II, Ryan Bakker, Royce Carroll, Christopher Hare, Keith T. Poole, Howard Rosenthal (2020)")
    packageStartupMessage("\n ## Pacakge creator: David A. Armstrong")
-   packageStartupMessage("\n ## Pacakge contributors: David A. Armstrong (https://github.com/davidaarmstrong/asmcjr), Yen-Chieh Liao (https://github.com/yl17124/asmcjr)")
+   packageStartupMessage("\n ## Pacakge contributors: David A. Armstrong (https://github.com/davidaarmstrong/asmcjr), David Liao (https://github.com/yl17124/asmcjr)")
    packageStartupMessage("\n ## Installation succeeded via https://github.com/yl17124/asmcjr on ", date())
 }
 
